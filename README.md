06407296465c6bb37792688f4caae19085d5b129
ValouBambou <theval358@gmail.com>
2022-09-11 00:05:47 +0200

    add install and cleaning commands to install scripts and update README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
 Some config for various software that I use on various machines.
+
+## Requirement
+ 
+Install [nvchad](https://nvchad.com/quickstart/install#pre-requisites) with it requierements (neovim 0.7.2 or above, nerd font, ripgrep)
+
--- config/nvchad/install.sh
+++ config/nvchad/install.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
+echo "Removing old nvim config to install nvchad"
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
+
+echo "Cloning nvchad"
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
+echo "Copy custom nvchad config"
 cp -r custom ~/.config/nvim/lua/
