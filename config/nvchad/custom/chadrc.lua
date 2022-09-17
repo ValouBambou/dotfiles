83f78e112b0ec1b841cea000e894307d7d8b1526
Valentin Trophime <theval358@gmail.com>
2022-09-17 19:17:07 +0200

    newer syntax and add gretter alpha-nvim plugin

--- config/nvchad/custom/chadrc.lua
+++ config/nvchad/custom/chadrc.lua
@@ -3,26 +3,11 @@ local M = {}
 
 -- make sure you maintain the structure of `core/default_config.lua` here,
 
-M.plugins = {
-  user = {
-    ["neovim/nvim-lspconfig"] = {
-      config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.plugins.lspconfig"
-      end,
-    },
-    ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-         require "custom.plugins.null-ls"
-      end,
-    }
-  },
-}
+M.plugins = require("custom.plugins")
 
 -- example of changing theme:
 M.ui = {
-  theme = "gruvchad",
+	theme = "gruvchad",
 }
 
 return M
