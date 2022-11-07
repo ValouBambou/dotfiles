7c74ad5348e9a5c4a6d09265ce3f30618cc5be08
Valentin Trophime <valentin.trophime@inria.fr>
2022-11-07 15:48:56 +0100

    use mason to install all lsp servers

--- config/nvchad/custom/plugins/init.lua
+++ config/nvchad/custom/plugins/init.lua
@@ -5,8 +5,30 @@ return {
       require "custom.plugins.lspconfig"
     end,
   },
+  ["williamboman/mason.nvim"] = {
+   override_options = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "json-lsp",
+
+        -- python
+        "pyright",
+        "black",
+
+        -- rust
+        "rust-analyzer",
+        -- shell
+        "shfmt",
+        "shellcheck",
+      },
+    },
+  },
   ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
+    after =     "nvim-lspconfig",
     config = function()
       require "custom.plugins.null-ls"
     end,
