f40c78a37b752be66efaacf197b1a6be191acb0a
Valentin Trophime <valentin.trophime@inria.fr>
2023-07-17 11:29:56 +0200

    add safety to update local (save old config in /tmp in case and ask confirmation)

--- update.sh
+++ update.sh
@@ -8,13 +8,19 @@ fi
 
 if [ $1 = "local" ]; then
     mkdir -p ~/.config 
+    read -e -p "You sure about overwriting the current ~/.config ? [y/N] " choice
+    if [ "$choice" != [Yy]* ]; then 
+        exit 0
+    fi
+    echo "Just in case backup the current config to /tmp"
+    cp -r ~/.config /tmp
     for dir in $(ls config)
     do
-        if [ $dir = "nvchad" ]; then
-            echo "Copying dotfiles/config/$dir/custom to ~/.config/nvim/lua/custom"
+        if [ "$dir" == "nvchad" ]; then
+            echo "dotfiles/config/$dir/custom -> ~/.config/nvim/lua/custom"
             cp -r config/nvchad/custom ~/.config/nvim/lua
         else 
-            echo "Copying dotfiles/config/$dir to ~/.config/$dir"
+            echo "dotfiles/config/$dir -> ~/.config/$dir"
             cp -r config/$dir ~/.config/$dir
         fi
             
@@ -25,11 +31,12 @@ fi
 if [ $1 = "remote" ]; then
     for dir in $(ls config)
     do
-        if [ $dir = "nvchad" ]; then
-            echo "Copying dotfiles/config/$dir/custom from ~/.config/nvim/lua/custom"
+        if [ "$dir" == "nvchad" ]; then
+            echo "dotfiles/config/$dir/custom <- ~/.config/nvim/lua/custom"
             cp -r  ~/.config/nvim/lua/custom config/nvchad
         else 
-            echo "Copying dotfiles/config/$dir from ~/.config/$dir"
+            echo "dotfiles/config/$dir <- ~/.config/$dir"
+            rm -rf config/$dir
             cp -r ~/.config/$dir config
         fi
     done
