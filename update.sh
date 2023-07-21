02a616a1b226fa2e5fb01961e0a7e2ed0bff7031
ValouBambou <theval358@gmail.com>
2023-07-21 18:11:23 +0200

    fix if brackets + replace dir instead of copying inside the previous one

--- update.sh
+++ update.sh
@@ -9,19 +9,19 @@ fi
 if [ $1 = "local" ]; then
     mkdir -p ~/.config 
     read -e -p "You sure about overwriting the current ~/.config ? [y/N] " choice
-    if [ "$choice" != [Yy]* ]; then 
+    if [[ "$choice" != [Yy]* ]]; then 
         exit 0
     fi
     echo "Just in case backup the current config to /tmp"
     cp -r ~/.config /tmp
     for dir in $(ls config)
     do
-        if [ "$dir" == "nvchad" ]; then
+        if [ "$dir" = "nvchad" ]; then
             echo "dotfiles/config/$dir/custom -> ~/.config/nvim/lua/custom"
             cp -r config/nvchad/custom ~/.config/nvim/lua
         else 
             echo "dotfiles/config/$dir -> ~/.config/$dir"
-            cp -r config/$dir ~/.config/$dir
+            cp -r config/$dir ~/.config
         fi
             
     done
